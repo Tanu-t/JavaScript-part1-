f0debe74c591ed37989412500326284a8e885560
Tanu Tyagi <136581425+Tanu-t@users.noreply.github.com>
2024-03-07 23:14:33 +0530

    Add files via upload

---
+++ 02_dataTypes.js
@@ -0,0 +1,31 @@
+"use strict"; //treat all JS code as newer version
+
+//alert(3+3) // we are using nodejs, not browser
+
+
+console.log(3 
+    +
+    3);   // code readability should be high
+
+//datatypes - 7 
+
+//(nn ss bb u) - number, null, string, symbol, boolean, bigInt, undefined
+
+let name = 'tanu'
+let age = 22
+let isLoggedIn = false
+let state;
+let temperature = null //null means empty temperature aaya he nhi hai
+
+
+//number => 2 to power 53
+//bigInt
+//string => ""
+//boolean => true/false
+//null => standalone value
+//undefined => 
+//symbol => unique
+
+
+
+
