d159bd543cb978d482b4aabce675e1eccbac3849
Tanu Tyagi <136581425+Tanu-t@users.noreply.github.com>
2024-03-07 23:47:51 +0530

    Add files via upload

---
+++ 04_conventionOperation.js
@@ -0,0 +1,23 @@
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2/2);
+// console.log(2**2);
+// console.log(2%2);
+
+
+// console.log("1" + 2);  //12
+// console.log(1 + "2");  // 12
+// console.log("1" + "2"); // 12
+// console.log("1" + 2 + 2); // 122
+// console.log(1 + 2 + "2"); // 32
+
+// prefix and postfix operator
+
+// x++
+// ++x
+
+// if used postfix, with operator after operand(for example, x++),the increment operator increments and returns the value before incrementing
+
+// if used prefix, with operator before operand(for example, ++x),the increment operator increments and returns the value after incrementing
